0d18bd55e79fdcdcf116a8bf19158a45db292dcd
luckyAkbar <luckyakbar1509@gmail.com>
2024-12-13 13:36:03 +0700

    feat: new utility to generate image for questionnaire result

--- internal/usecase/questionnaire.go
+++ internal/usecase/questionnaire.go
@@ -244,3 +244,245 @@ func (u *QuestionnaireUsecase) HandleSubmitQuestionnaire(ctx context.Context, in
 		CreatedBy: result.CreatedBy,
 	}, nil
 }
+
+type imageGenerator struct {
+	Title          string
+	Result         model.ResultDetail
+	TestID         uuid.UUID
+	IndicationText string
+
+	rgba         *image.RGBA
+	ttp          []string // ttp stands for text to print
+	width        int
+	height       int
+	titleDrawer  *font.Drawer
+	textDrawer   *font.Drawer
+	sampleDrawer *font.Drawer
+	font         *truetype.Font
+	dpi          float64
+	textSize     float64
+	titleSize    float64
+	spacing      float64
+}
+
+type imageGenerationOpts struct {
+	Title          string
+	Result         model.ResultDetail
+	TestID         uuid.UUID
+	IndicationText string
+}
+
+type imageResult struct {
+	ContentType string
+	Buffer      bytes.Buffer
+}
+
+//nolint:mnd
+func newImageGenerator(f *truetype.Font, opts imageGenerationOpts) *imageGenerator {
+	dpi := float64(208)
+	size := float64(12)
+	spacing := float64(1.5)
+	titleSize := float64(18)
+
+	initialTitleDrawer := &font.Drawer{
+		Face: truetype.NewFace(f, &truetype.Options{
+			Size:    titleSize,
+			DPI:     dpi,
+			Hinting: font.HintingFull,
+		}),
+	}
+
+	initialTextDrawer := &font.Drawer{
+		Face: truetype.NewFace(f, &truetype.Options{
+			Size:    size,
+			DPI:     dpi,
+			Hinting: font.HintingNone,
+		}),
+	}
+
+	imgGenerator := &imageGenerator{
+		Title:          opts.Title,
+		Result:         opts.Result,
+		TestID:         opts.TestID,
+		IndicationText: opts.IndicationText,
+		sampleDrawer:   initialTextDrawer,
+		spacing:        spacing,
+		font:           f,
+		textSize:       size,
+		titleSize:      titleSize,
+		dpi:            dpi,
+	}
+
+	imgGenerator.generateTTP()
+	imgGenerator.countOptimumImageWidth(initialTextDrawer, initialTitleDrawer)
+	imgGenerator.countOptimumImageHeight()
+
+	rgba := image.NewRGBA(image.Rect(0, 0, imgGenerator.width, imgGenerator.height))
+	draw.Draw(rgba, rgba.Bounds(), image.White, image.Point{}, draw.Src)
+	imgGenerator.rgba = rgba
+	imgGenerator.generateTextDrawer()
+	imgGenerator.generateTitleDrawer()
+
+	return imgGenerator
+}
+
+// GenerateJPEG will generate jpeg image for the test result
+//
+//nolint:mnd
+func (ig *imageGenerator) GenerateJPEG() *imageResult {
+	y := 10 + int(math.Ceil(ig.textSize*ig.dpi/72))
+	dy := int(math.Ceil(ig.textSize * ig.spacing * ig.dpi / 72))
+	ig.textDrawer.Dot = fixed.Point26_6{
+		X: (fixed.I(ig.width) - ig.textDrawer.MeasureString(ig.Title)) / 2,
+		Y: fixed.I(y),
+	}
+
+	ty := 10 + int(math.Ceil(ig.titleSize*ig.dpi/72))
+	tdy := int(math.Ceil(ig.titleSize * ig.spacing * ig.dpi / 72))
+
+	tx := (fixed.I(ig.width) - ig.titleDrawer.MeasureString(ig.Title)) / 2
+
+	ig.titleDrawer.Dot = fixed.Point26_6{
+		X: tx,
+		Y: fixed.I(ty),
+	}
+
+	ig.titleDrawer.DrawString(ig.Title)
+
+	y += tdy
+
+	for _, s := range ig.ttp {
+		center := (fixed.I(ig.width) - ig.textDrawer.MeasureString(s)) / 2
+		ig.textDrawer.Dot = fixed.P(center.Ceil(), y)
+		ig.textDrawer.DrawString(s)
+
+		y += dy
+	}
+
+	var imgBuf bytes.Buffer
+	if err := jpeg.Encode(&imgBuf, ig.rgba, nil); err != nil {
+		logrus.WithError(err).Error("failed to encode image")
+	}
+
+	return &imageResult{
+		ContentType: "image/jpeg",
+		Buffer:      imgBuf,
+	}
+}
+
+const optimumTextLength = 65
+
+func (ig *imageGenerator) generateTTP() {
+	total := 0
+
+	for _, r := range ig.Result {
+		ig.appendTTP(fmt.Sprintf("%s: %d", r.Name, r.Grade))
+		total += r.Grade
+	}
+
+	ig.appendTTP(fmt.Sprintf("Total: %d", total))
+	ig.appendTTP("Indikasi: " + ig.IndicationText)
+	ig.appendTTP(fmt.Sprintf("Test ID: %s", ig.TestID))
+}
+
+func (ig *imageGenerator) appendTTP(s string) {
+	arrStr := ig.ensureSafeLongText(s, ig.sampleDrawer)
+	ig.ttp = append(ig.ttp, arrStr...)
+}
+
+// maxImageWidth are made to limit the maximum image output width
+const maxImageWitdh = 1080
+
+//nolint:mnd
+func (ig *imageGenerator) countOptimumImageWidth(initialTextDrawer, initialTitleDrawer *font.Drawer) {
+	maxWidth := initialTitleDrawer.MeasureString(ig.Title)
+
+	for _, t := range ig.ttp {
+		ms := initialTextDrawer.MeasureString(t)
+		if ms > maxWidth {
+			maxWidth = ms
+		}
+	}
+
+	if maxWidth >= maxImageWitdh {
+		ig.width = maxImageWitdh
+	} else {
+		ig.width = maxWidth.Ceil() + 5*maxWidth.Ceil()/100
+	}
+}
+
+// ensureSafeLongText will try to check if writing s will cause text overflow
+// or if the text length more than optimumTextLength.
+// If text deemed to long, will call wordWrapper to wrap the text and prevent overflow
+func (ig *imageGenerator) ensureSafeLongText(s string, drawer *font.Drawer) []string {
+	width := drawer.MeasureString(s)
+	if width.Ceil() >= maxImageWitdh || len(s) >= optimumTextLength {
+		return wordWrapper(s)
+	}
+
+	return []string{s}
+}
+
+func wordWrapper(s string) []string {
+	if strings.TrimSpace(s) == "" {
+		return []string{s}
+	}
+
+	result := []string{}
+	ss := strings.Fields(s)
+	appended := false
+
+	var temp string
+
+	for i, word := range ss {
+		temp += word + " "
+		if len(temp) >= optimumTextLength {
+			result = append(result, temp)
+			temp = ""
+			appended = true
+		}
+
+		if i == len(ss)-1 && !appended {
+			result = append(result, temp)
+			break
+		}
+	}
+
+	return result
+}
+
+//nolint:mnd
+func (ig *imageGenerator) countOptimumImageHeight() {
+	y := 10 + int(math.Ceil(ig.textSize*ig.dpi/72))
+	tdy := int(math.Ceil(ig.titleSize * ig.spacing * ig.dpi / 72))
+	y += tdy
+
+	incrementor := int(math.Ceil(ig.textSize * ig.spacing * ig.dpi / 72))
+	y += incrementor * len(ig.ttp)
+
+	ig.height = y
+}
+
+func (ig *imageGenerator) generateTextDrawer() {
+	ig.textDrawer = &font.Drawer{
+		Dst: ig.rgba,
+		Src: image.Black,
+		Face: truetype.NewFace(ig.font, &truetype.Options{
+			Size:    ig.textSize,
+			DPI:     ig.dpi,
+			Hinting: font.HintingNone,
+		}),
+	}
+}
+
+func (ig *imageGenerator) generateTitleDrawer() {
+	ig.titleDrawer = &font.Drawer{
+		Dst: ig.rgba,
+		Src: image.Black,
+		Face: truetype.NewFace(ig.font, &truetype.Options{
+			Size:    ig.titleSize,
+			DPI:     ig.dpi,
+			Hinting: font.HintingFull,
+		}),
+	}
+}
