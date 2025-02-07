6b3b0568498609f83e969dc184dbc168de479e4a
luckyAkbar <luckyakbar1509@gmail.com>
2025-02-07 20:31:56 +0700

    fix: date of birth input

--- internal/delivery/rest/rest.go
+++ internal/delivery/rest/rest.go
@@ -59,7 +59,7 @@ func (s *service) initV1Routes() {
 	s.v1.GET("/atec/packages/active", s.HandleSearchActivePackage())
 
 	s.v1.POST("/childern", s.HandleRegisterChildern(), s.AuthMiddleware(true, false))
-	s.v1.PUT("/childern/:child_id", s.HandleUpdateChildern())
+	s.v1.PUT("/childern/:child_id", s.HandleUpdateChildern(), s.AuthMiddleware(true, false))
 	s.v1.GET("/childern", s.HandleGetMyChildern(), s.AuthMiddleware(true, false))
 	s.v1.GET("/childern/search", s.HandleSearchChildern(), s.AuthMiddleware(false, true))
 	s.v1.GET("/childern/:child_id/stats", s.HandleGetChildStats(), s.AuthMiddleware(true, false))
