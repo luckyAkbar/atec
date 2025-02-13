c307b817f7f08850cca6508aef500d98f7da3d02
luckyAkbar <luckyakbar1509@gmail.com>
2025-02-13 12:11:48 +0700

    initial test file

---
+++ internal/model/auth_test.go
@@ -0,0 +1,34 @@
+package model_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/luckyAkbar/atec/internal/model"
+
+	"github.com/google/uuid"
+)
+
+func TestSetUserToCtx(t *testing.T) {
+	user := model.AuthUser{
+		ID:   uuid.New(),
+		Role: model.RolesAdmin,
+	}
+
+	ctx := context.Background()
+	newCtx := model.SetUserToCtx(ctx, user)
+
+	got := model.GetUserFromCtx(newCtx)
+	if got == nil {
+		t.Errorf("expected user to be set in context, but got nil")
+		t.FailNow()
+	}
+
+	if got.ID != user.ID {
+		t.Errorf("expected user id to be %v, but got %v", user.ID, got.ID)
+	}
+
+	if got.Role != user.Role {
+		t.Errorf("expected user role to be %v, but got %v", user.Role, got.Role)
+	}
+}
