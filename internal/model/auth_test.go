3fe4db04330a625f270c89e165f7ee1bdb1d436b
luckyAkbar <luckyakbar1509@gmail.com>
2025-02-13 14:20:17 +0700

    test: add test for get user from ctx

--- internal/model/auth_test.go
+++ internal/model/auth_test.go
@@ -34,3 +34,39 @@ func TestSetUserToCtx(t *testing.T) {
 		t.Errorf("expected user role to be %v, but got %v", user.Role, got.Role)
 	}
 }
+
+func TestGetUserFromCtx(t *testing.T) {
+	t.Parallel()
+
+	user := model.AuthUser{
+		ID:   uuid.New(),
+		Role: model.RolesAdmin,
+	}
+
+	ctx := context.Background()
+	newCtx := model.SetUserToCtx(ctx, user)
+
+	t.Run("should correctly read the data", func(t *testing.T) {
+		got := model.GetUserFromCtx(newCtx)
+		if got == nil {
+			t.Errorf("user data is already set, should be able to read it")
+			t.FailNow()
+		}
+
+		if got.ID != user.ID {
+			t.Errorf("expected user id to be %v, but got %v", user.ID, got.ID)
+		}
+
+		if got.Role != user.Role {
+			t.Errorf("expected user role to be %v, but got %v", user.Role, got.Role)
+		}
+	})
+
+	t.Run("if the user data is not in the ctx, must return nil", func(t *testing.T) {
+		emptyCtx := context.Background()
+		got := model.GetUserFromCtx(emptyCtx)
+		if got != nil {
+			t.Errorf("on empty context value, should return nil")
+		}
+	})
+}
