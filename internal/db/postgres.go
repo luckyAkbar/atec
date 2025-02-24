b25c9f89fb61564e4e16d760718aebe39caf2a6c
luckyAkbar <luckyakbar1509@gmail.com>
2025-02-24 15:44:09 +0700

    feat: implement database connection pool

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -2,6 +2,7 @@
 package db
 
 import (
+	"log"
 	"os"
 	"time"
 
@@ -11,7 +12,7 @@ import (
 	libdb "github.com/sweet-go/stdlib/db"
 	"gorm.io/gorm"
 
-	gormLogger "gorm.io/gorm/logger"
+	"gorm.io/gorm/logger"
 )
 
 var (
@@ -40,19 +41,46 @@ func InitializePostgresConn() {
 		}
 	}
 
+	sqlDB, err := PostgresDB.DB()
+	if err != nil {
+		logrus.WithError(err).Error("failed to get postgres connection")
+		os.Exit(1)
+	}
+
+	sqlDB.SetMaxIdleConns(config.DBMaxIdleConn())
+	sqlDB.SetMaxOpenConns(config.DBMaxOpenConn())
+	sqlDB.SetConnMaxLifetime(config.DBConnMaxLifetime())
+
 	if !success {
 		logrus.Error("failed to initialize postgres connection")
 		os.Exit(1)
 	}
 
+	var logLevel logger.LogLevel
+
 	switch config.LogLevel() {
 	case "error":
-		PostgresDB.Logger = PostgresDB.Logger.LogMode(gormLogger.Error)
+		logLevel = logger.Error
 	case "warn":
-		PostgresDB.Logger = PostgresDB.Logger.LogMode(gormLogger.Warn)
+		logLevel = logger.Warn
 	default:
-		PostgresDB.Logger = PostgresDB.Logger.LogMode(gormLogger.Info)
+		logLevel = logger.Info
 	}
+
+	const defaultSlowThreshold = 500 * time.Millisecond
+
+	gormLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             defaultSlowThreshold, // Slow SQL threshold
+			LogLevel:                  logLevel,             // Log level
+			IgnoreRecordNotFoundError: true,                 // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,                 // Don't include params in the SQL log
+			Colorful:                  true,                 // Disable color
+		},
+	)
+
+	PostgresDB.Logger = gormLogger
 
 	logrus.Info("Connected to Postgres Database")
 }
