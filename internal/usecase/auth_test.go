8900110308cd62a4282d09f197a79a08b15be9fb
luckyAkbar <luckyakbar1509@gmail.com>
2025-03-23 20:50:55 +0700

    test: unit test for auth usecase HandleLogin

--- internal/usecase/auth_test.go
+++ internal/usecase/auth_test.go
@@ -1,9 +1,17 @@
 package usecase_test
 
 import (
+	"context"
 	"testing"
 
+	"github.com/google/uuid"
+	"github.com/luckyAkbar/atec/internal/model"
 	"github.com/luckyAkbar/atec/internal/usecase"
+	mockCommon "github.com/luckyAkbar/atec/mocks/internal_/common"
+	mockUsecase "github.com/luckyAkbar/atec/mocks/internal_/usecase"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLoginInput(t *testing.T) {
@@ -246,4 +254,201 @@ func TestResetPasswordInput(t *testing.T) {
 			t.Errorf("expecting nil but got %v", err)
 		}
 	})
+}
+
+func TestAuthUsecase_HandleLogin(t *testing.T) {
+	ctx := context.Background()
+	mockUserRepo := mockUsecase.NewUserRepository(t)
+	mockSharedCryptor := mockCommon.NewSharedCryptorIface(t)
+	uc := usecase.NewAuthUsecase(mockSharedCryptor, mockUserRepo, nil, nil, nil)
+	validSampleEmail := "valid@sample.email"
+	validSamplePassword := "validPass!!"
+	sampleEncryptedEmail := "encryptedEmail"
+	sampleJWTToken := "jwtToken"
+	user := model.User{
+		ID:       uuid.New(),
+		IsActive: true,
+	}
+
+	testCases := []struct {
+		name                 string
+		input                usecase.LoginInput
+		wantErr              bool
+		expectedErr          error
+		expectedOutput       *usecase.LoginOutput
+		expectedFunctionCall func()
+	}{
+		{
+			name: "invalid input, email is empty",
+			input: usecase.LoginInput{
+				Email:    "",
+				Password: validSamplePassword,
+			},
+			wantErr:     true,
+			expectedErr: usecase.ErrBadRequest,
+		},
+		{
+			name: "invalid email format",
+			input: usecase.LoginInput{
+				Email:    "this@isnotemail",
+				Password: validSamplePassword,
+			},
+			wantErr:     true,
+			expectedErr: usecase.ErrBadRequest,
+		},
+		{
+			name: "password must atleast be 8 chars",
+			input: usecase.LoginInput{
+				Email:    validSampleEmail,
+				Password: "missing",
+			},
+			wantErr:     true,
+			expectedErr: usecase.ErrBadRequest,
+		},
+		{
+			name: "system failed to encrypt the email",
+			input: usecase.LoginInput{
+				Email:    validSampleEmail,
+				Password: validSamplePassword,
+			},
+			wantErr:     true,
+			expectedErr: usecase.ErrInternal,
+			expectedFunctionCall: func() {
+				mockSharedCryptor.EXPECT().Encrypt(validSampleEmail).Return("", assert.AnError).Once()
+			},
+		},
+		{
+			name: "system failed to query to database for user by email",
+			input: usecase.LoginInput{
+				Email:    validSampleEmail,
+				Password: validSamplePassword,
+			},
+			wantErr:     true,
+			expectedErr: usecase.ErrInternal,
+			expectedFunctionCall: func() {
+				mockSharedCryptor.EXPECT().Encrypt(validSampleEmail).Return(sampleEncryptedEmail, nil).Once()
+				mockUserRepo.EXPECT().FindByEmail(ctx, sampleEncryptedEmail).Return(nil, assert.AnError).Once()
+			},
+		},
+		{
+			name: "no user was found with the supplied email",
+			input: usecase.LoginInput{
+				Email:    validSampleEmail,
+				Password: validSamplePassword,
+			},
+			wantErr:     true,
+			expectedErr: usecase.ErrNotFound,
+			expectedFunctionCall: func() {
+				mockSharedCryptor.EXPECT().Encrypt(validSampleEmail).Return(sampleEncryptedEmail, nil).Once()
+				mockUserRepo.EXPECT().FindByEmail(ctx, sampleEncryptedEmail).Return(nil, usecase.ErrRepoNotFound).Once()
+			},
+		},
+		{
+			name: "inactive user shouldn't be able to login",
+			input: usecase.LoginInput{
+				Email:    validSampleEmail,
+				Password: validSamplePassword,
+			},
+			wantErr:     true,
+			expectedErr: usecase.ErrUnauthorized,
+			expectedFunctionCall: func() {
+				mockSharedCryptor.EXPECT().Encrypt(validSampleEmail).Return(sampleEncryptedEmail, nil).Once()
+
+				inactiveUser := user
+				inactiveUser.IsActive = false
+
+				mockUserRepo.EXPECT().FindByEmail(ctx, sampleEncryptedEmail).Return(&inactiveUser, nil).Once()
+			},
+		},
+		{
+			name: "inactive user shouldn't be able to login",
+			input: usecase.LoginInput{
+				Email:    validSampleEmail,
+				Password: validSamplePassword,
+			},
+			wantErr:     true,
+			expectedErr: usecase.ErrUnauthorized,
+			expectedFunctionCall: func() {
+				mockSharedCryptor.EXPECT().Encrypt(validSampleEmail).Return(sampleEncryptedEmail, nil).Once()
+
+				inactiveUser := user
+				inactiveUser.IsActive = false
+
+				mockUserRepo.EXPECT().FindByEmail(ctx, sampleEncryptedEmail).Return(&inactiveUser, nil).Once()
+			},
+		},
+		{
+			name: "failure to compare hash means unauthorized login",
+			input: usecase.LoginInput{
+				Email:    validSampleEmail,
+				Password: validSamplePassword,
+			},
+			wantErr:     true,
+			expectedErr: usecase.ErrUnauthorized,
+			expectedFunctionCall: func() {
+				mockSharedCryptor.EXPECT().Encrypt(validSampleEmail).Return(sampleEncryptedEmail, nil).Once()
+				mockUserRepo.EXPECT().FindByEmail(ctx, sampleEncryptedEmail).Return(&user, nil).Once()
+				mockSharedCryptor.EXPECT().CompareHash(mock.Anything, []byte(validSamplePassword)).Return(assert.AnError).Once()
+			},
+		},
+		{
+			name: "failure to create JWT must return internal error",
+			input: usecase.LoginInput{
+				Email:    validSampleEmail,
+				Password: validSamplePassword,
+			},
+			wantErr:     true,
+			expectedErr: usecase.ErrInternal,
+			expectedFunctionCall: func() {
+				mockSharedCryptor.EXPECT().Encrypt(validSampleEmail).Return(sampleEncryptedEmail, nil).Once()
+				mockUserRepo.EXPECT().FindByEmail(ctx, sampleEncryptedEmail).Return(&user, nil).Once()
+				mockSharedCryptor.EXPECT().CompareHash(mock.Anything, []byte(validSamplePassword)).Return(nil).Once()
+				mockSharedCryptor.EXPECT().CreateJWT(mock.Anything).Return("", assert.AnError).Once()
+			},
+		},
+		{
+			name: "ok",
+			input: usecase.LoginInput{
+				Email:    validSampleEmail,
+				Password: validSamplePassword,
+			},
+			wantErr:     false,
+			expectedErr: usecase.ErrInternal,
+			expectedOutput: &usecase.LoginOutput{
+				Token: sampleJWTToken,
+			},
+			expectedFunctionCall: func() {
+				mockSharedCryptor.EXPECT().Encrypt(validSampleEmail).Return(sampleEncryptedEmail, nil).Once()
+				mockUserRepo.EXPECT().FindByEmail(ctx, sampleEncryptedEmail).Return(&user, nil).Once()
+				mockSharedCryptor.EXPECT().CompareHash(mock.Anything, []byte(validSamplePassword)).Return(nil).Once()
+				mockSharedCryptor.EXPECT().CreateJWT(mock.Anything).Return(sampleJWTToken, nil).Once()
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.expectedFunctionCall != nil {
+				tc.expectedFunctionCall()
+			}
+
+			res, err := uc.HandleLogin(ctx, tc.input)
+
+			if !tc.wantErr {
+				require.NoError(t, err)
+				assert.Equal(t, tc.expectedOutput, res)
+
+				return
+			}
+
+			require.Error(t, err)
+
+			switch e := err.(type) {
+			default:
+				t.Errorf("expecting usecase error but got %T", err)
+			case usecase.UsecaseError:
+				assert.Equal(t, tc.expectedErr, e.ErrType)
+			}
+		})
+	}
 }
