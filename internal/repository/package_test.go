dd81038e28a61724a8910b8f674244a85b344889
luckyAkbar <luckyakbar1509@gmail.com>
2025-05-09 20:04:22 +0700

    test: more unit test for repository

--- internal/repository/package_test.go
+++ internal/repository/package_test.go
@@ -7,12 +7,429 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/google/uuid"
+	"github.com/luckyAkbar/atec/internal/common"
+	"github.com/luckyAkbar/atec/internal/db"
+	"github.com/luckyAkbar/atec/internal/model"
 	"github.com/luckyAkbar/atec/internal/repository"
 	"github.com/luckyAkbar/atec/internal/usecase"
+	common_mock "github.com/luckyAkbar/atec/mocks/internal_/common"
+	db_mock "github.com/luckyAkbar/atec/mocks/internal_/db"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"gorm.io/gorm"
 )
+
+func TestPackageRepository_Create(t *testing.T) {
+	ctx := context.Background()
+	kit, closer := InitializeRepoTestKit(t)
+
+	defer closer()
+
+	dbMock := kit.DBmock
+	cacher := db_mock.NewCacheKeeperIface(t)
+	redsyncMutex := common_mock.NewRedsyncMutex(t)
+	repo := repository.NewPackageRepo(kit.DB, cacher)
+
+	mutexWrapper := common.NewRedsyncMutexWrapper(redsyncMutex)
+
+	testCases := []struct {
+		name                 string
+		wantErr              bool
+		input                usecase.RepoCreatePackageInput
+		expectedErr          error
+		expectedFunctionCall func()
+	}{
+		{
+			name:        "unexpected db error when trying to submit",
+			wantErr:     true,
+			input:       usecase.RepoCreatePackageInput{},
+			expectedErr: assert.AnError,
+			expectedFunctionCall: func() {
+				dbMock.ExpectBegin()
+				dbMock.ExpectQuery(`^INSERT INTO "packages"`).
+					WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+					WillReturnError(assert.AnError)
+				dbMock.ExpectRollback()
+			},
+		},
+		{
+			name:        "failed to acquire lock doesn't returning error",
+			wantErr:     false,
+			input:       usecase.RepoCreatePackageInput{},
+			expectedErr: assert.AnError,
+			expectedFunctionCall: func() {
+				dbMock.ExpectBegin()
+				dbMock.ExpectQuery(`^INSERT INTO "packages"`).
+					WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+					WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+				dbMock.ExpectCommit()
+
+				cacher.EXPECT().AcquireLock(mock.Anything).Return(nil, assert.AnError).Once()
+			},
+		},
+		{
+			name:    "failed to set cache to JSON, but OK",
+			wantErr: false,
+			input:   usecase.RepoCreatePackageInput{},
+			expectedFunctionCall: func() {
+				dbMock.ExpectBegin()
+				dbMock.ExpectQuery(`^INSERT INTO "packages"`).
+					WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+					WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+				dbMock.ExpectCommit()
+
+				cacher.EXPECT().AcquireLock(mock.Anything).Return(mutexWrapper, nil).Once()
+				redsyncMutex.EXPECT().Unlock().Return(true, nil).Once()
+				cacher.EXPECT().SetJSON(ctx, mock.Anything, mock.Anything, mock.Anything).Return(assert.AnError).Once()
+			},
+		},
+		{
+			name:    "failed to unlock, but OK",
+			wantErr: false,
+			input:   usecase.RepoCreatePackageInput{},
+			expectedFunctionCall: func() {
+				dbMock.ExpectBegin()
+				dbMock.ExpectQuery(`^INSERT INTO "packages"`).
+					WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+					WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+				dbMock.ExpectCommit()
+
+				cacher.EXPECT().AcquireLock(mock.Anything).Return(mutexWrapper, nil).Once()
+				redsyncMutex.EXPECT().Unlock().Return(false, assert.AnError).Once()
+				cacher.EXPECT().SetJSON(ctx, mock.Anything, mock.Anything, mock.Anything).Return(nil).Once()
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.expectedFunctionCall != nil {
+				tc.expectedFunctionCall()
+			}
+
+			res, err := repo.Create(ctx, tc.input, kit.DB)
+
+			if tc.wantErr {
+				require.Error(t, err)
+				require.Nil(t, res)
+
+				if tc.expectedErr != nil {
+					assert.Equal(t, tc.expectedErr, err)
+				}
+
+				return
+			}
+
+			require.NoError(t, err)
+			assert.NotNil(t, res)
+		})
+	}
+}
+
+func TestPackageRepository_FindByID(t *testing.T) {
+	ctx := context.Background()
+	kit, closer := InitializeRepoTestKit(t)
+
+	defer closer()
+
+	// dbMock := kit.DBmock
+	cacher := db_mock.NewCacheKeeperIface(t)
+	repo := repository.NewPackageRepo(kit.DB, cacher)
+	id := uuid.New()
+
+	redsyncMutex := common_mock.NewRedsyncMutex(t)
+	mutexWrapper := common.NewRedsyncMutexWrapper(redsyncMutex)
+
+	testCases := []struct {
+		name                 string
+		wantErr              bool
+		expectedErr          error
+		expectedFunctionCall func()
+	}{
+		{
+			name:    "failed to get or lock the cache by key",
+			wantErr: true,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(mock.Anything, mock.Anything).Return("!", nil, assert.AnError).Once()
+			},
+		},
+		{
+			name:        "cache is set to nil",
+			wantErr:     true,
+			expectedErr: repository.ErrNotFound,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(mock.Anything, mock.Anything).Return("!", nil, db.ErrCacheNil).Once()
+			},
+		},
+		{
+			name:        "cache took too long",
+			wantErr:     true,
+			expectedErr: repository.ErrTimeout,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(mock.Anything, mock.Anything).Return("!", nil, db.ErrLockWaitingTooLong).Once()
+			},
+		},
+		{
+			name:    "failed unmarshalling the value",
+			wantErr: true,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(mock.Anything, mock.Anything).Return("!", nil, nil).Once()
+			},
+		},
+		{
+			name:    "got marshalled value from cache",
+			wantErr: false,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(mock.Anything, mock.Anything).Return("{}", nil, nil).Once()
+			},
+		},
+		{
+			name:    "when fetching from db, got error",
+			wantErr: true,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(mock.Anything, mock.Anything).Return("", mutexWrapper, nil).Once()
+				kit.DBmock.ExpectQuery(`^SELECT .+ FROM "packages"`).
+					WithArgs(id, 1).
+					WillReturnError(assert.AnError)
+				redsyncMutex.EXPECT().Unlock().Return(true, nil).Once()
+			},
+		},
+		{
+			name:    "when fetching from db, got error, also failed to unlock",
+			wantErr: true,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(mock.Anything, mock.Anything).Return("", mutexWrapper, nil).Once()
+				kit.DBmock.ExpectQuery(`^SELECT .+ FROM "packages"`).
+					WithArgs(id, 1).
+					WillReturnError(assert.AnError)
+				redsyncMutex.EXPECT().Unlock().Return(false, assert.AnError).Once()
+			},
+		},
+		{
+			name:    "ok - all good",
+			wantErr: false,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(mock.Anything, mock.Anything).Return("", mutexWrapper, nil).Once()
+				kit.DBmock.ExpectQuery(`^SELECT .+ FROM "packages"`).
+					WithArgs(id, 1).
+					WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+				redsyncMutex.EXPECT().Unlock().Return(true, nil).Once()
+				cacher.EXPECT().SetJSON(ctx, mock.Anything, mock.Anything, mock.Anything).Return(nil).Once()
+			},
+		},
+		{
+			name:    "the package wasn't found on db",
+			wantErr: true,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(mock.Anything, mock.Anything).Return("", mutexWrapper, nil).Once()
+				kit.DBmock.ExpectQuery(`^SELECT .+ FROM "packages"`).
+					WithArgs(id, 1).
+					WillReturnError(gorm.ErrRecordNotFound)
+				redsyncMutex.EXPECT().Unlock().Return(true, nil).Once()
+				cacher.EXPECT().SetNil(ctx, mock.Anything, mock.Anything).Return(nil).Once()
+			},
+		},
+		{
+			name:    "the package wasn't found on db - failed to set nil to cache",
+			wantErr: true,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(mock.Anything, mock.Anything).Return("", mutexWrapper, nil).Once()
+				kit.DBmock.ExpectQuery(`^SELECT .+ FROM "packages"`).
+					WithArgs(id, 1).
+					WillReturnError(gorm.ErrRecordNotFound)
+				redsyncMutex.EXPECT().Unlock().Return(true, nil).Once()
+				cacher.EXPECT().SetNil(ctx, mock.Anything, mock.Anything).Return(assert.AnError).Once()
+			},
+		},
+		{
+			name:    "ok - failed to set json",
+			wantErr: false,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(mock.Anything, mock.Anything).Return("", mutexWrapper, nil).Once()
+				kit.DBmock.ExpectQuery(`^SELECT .+ FROM "packages"`).
+					WithArgs(id, 1).
+					WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+				redsyncMutex.EXPECT().Unlock().Return(true, nil).Once()
+				cacher.EXPECT().SetJSON(ctx, mock.Anything, mock.Anything, mock.Anything).Return(assert.AnError).Once()
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.expectedFunctionCall != nil {
+				tc.expectedFunctionCall()
+			}
+
+			res, err := repo.FindByID(ctx, id)
+
+			if tc.wantErr {
+				require.Error(t, err)
+				require.Nil(t, res)
+
+				if tc.expectedErr != nil {
+					t.Logf("Got error: %v (type: %T), Expected error: %v (type: %T)", err, err, tc.expectedErr, tc.expectedErr)
+					assert.Equal(t, tc.expectedErr, err)
+				}
+
+				return
+			}
+
+			require.NoError(t, err)
+			assert.NotNil(t, res)
+		})
+	}
+}
+
+func TestPackageRepository_Update(t *testing.T) {
+	ctx := context.Background()
+	kit, closer := InitializeRepoTestKit(t)
+
+	defer closer()
+
+	// dbMock := kit.DBmock
+	cacher := db_mock.NewCacheKeeperIface(t)
+	repo := repository.NewPackageRepo(kit.DB, cacher)
+	id := uuid.New()
+
+	redsyncMutex := common_mock.NewRedsyncMutex(t)
+	mutexWrapper := common.NewRedsyncMutexWrapper(redsyncMutex)
+
+	activeStatus := true
+	lockStatus := true
+	packageName := "package name"
+	questionnaire := &model.Questionnaire{}
+
+	testCases := []struct {
+		name                 string
+		wantErr              bool
+		input                usecase.RepoUpdatePackageInput
+		expectedErr          error
+		expectedFunctionCall func()
+	}{
+		{
+			name:    "failed to acquire lock",
+			wantErr: true,
+			input:   usecase.RepoUpdatePackageInput{},
+			expectedFunctionCall: func() {
+				cacher.EXPECT().AcquireLock(mock.Anything).Return(nil, assert.AnError).Once()
+			},
+		},
+		{
+			name:    "when failed to delete stale data from cache, must return error",
+			wantErr: true,
+			input:   usecase.RepoUpdatePackageInput{},
+			expectedFunctionCall: func() {
+				cacher.EXPECT().AcquireLock(mock.Anything).Return(mutexWrapper, nil).Once()
+				redsyncMutex.EXPECT().Unlock().Return(true, nil).Once()
+				cacher.EXPECT().Del(ctx, mock.Anything).Return(assert.AnError).Once()
+			},
+		},
+		{
+			name:    "when failed to delete stale data from cache, must return error - failed to unlock",
+			wantErr: true,
+			input:   usecase.RepoUpdatePackageInput{},
+			expectedFunctionCall: func() {
+				cacher.EXPECT().AcquireLock(mock.Anything).Return(mutexWrapper, nil).Once()
+				redsyncMutex.EXPECT().Unlock().Return(false, assert.AnError).Once()
+				cacher.EXPECT().Del(ctx, mock.Anything).Return(assert.AnError).Once()
+			},
+		},
+		{
+			name:    "got error from db when updating the package",
+			wantErr: true,
+			input: usecase.RepoUpdatePackageInput{
+				ActiveStatus:  &activeStatus,
+				LockStatus:    &lockStatus,
+				PackageName:   packageName,
+				Questionnaire: questionnaire,
+			},
+			expectedFunctionCall: func() {
+				cacher.EXPECT().AcquireLock(mock.Anything).Return(mutexWrapper, nil).Once()
+				redsyncMutex.EXPECT().Unlock().Return(true, nil).Once()
+				cacher.EXPECT().Del(ctx, mock.Anything).Return(nil).Once()
+
+				kit.DBmock.ExpectQuery(`^UPDATE "packages"`).
+					WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+					WillReturnError(assert.AnError)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.expectedFunctionCall != nil {
+				tc.expectedFunctionCall()
+			}
+
+			res, err := repo.Update(ctx, id, tc.input, kit.DB)
+
+			if tc.wantErr {
+				require.Error(t, err)
+				require.Nil(t, res)
+
+				if tc.expectedErr != nil {
+					assert.Equal(t, tc.expectedErr, err)
+				}
+
+				return
+			}
+
+			require.NoError(t, err)
+			assert.NotNil(t, res)
+		})
+	}
+}
+
+func TestPackageRepository_Delete(t *testing.T) {
+	ctx := context.Background()
+	kit, closer := InitializeRepoTestKit(t)
+
+	defer closer()
+
+	// dbMock := kit.DBmock
+	cacher := db_mock.NewCacheKeeperIface(t)
+	repo := repository.NewPackageRepo(kit.DB, cacher)
+	id := uuid.New()
+
+	testCases := []struct {
+		name                 string
+		wantErr              bool
+		expectedErr          error
+		expectedFunctionCall func()
+	}{
+		{
+			name:    "failed to acquire lock",
+			wantErr: true,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().AcquireLock(mock.Anything).Return(nil, assert.AnError).Once()
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.expectedFunctionCall != nil {
+				tc.expectedFunctionCall()
+			}
+
+			err := repo.Delete(ctx, id)
+
+			if tc.wantErr {
+				require.Error(t, err)
+
+				if tc.expectedErr != nil {
+					assert.Equal(t, tc.expectedErr, err)
+				}
+
+				return
+			}
+
+			require.NoError(t, err)
+		})
+	}
+}
 
 func TestPackageRepository_Search(t *testing.T) {
 	ctx := context.Background()
@@ -158,6 +575,85 @@ func TestPackageRepository_FindOldestActiveAndLockedPackage(t *testing.T) {
 			}
 
 			res, err := repo.FindOldestActiveAndLockedPackage(ctx)
+
+			if tc.wantErr {
+				require.Error(t, err)
+				require.Nil(t, res)
+
+				if tc.expectedErr != nil {
+					assert.Equal(t, tc.expectedErr, err)
+				}
+
+				return
+			}
+
+			require.NoError(t, err)
+			assert.NotNil(t, res)
+		})
+	}
+}
+
+func TestPackageRepository_FindAllActivePackages(t *testing.T) {
+	ctx := context.Background()
+	kit, closer := InitializeRepoTestKit(t)
+
+	defer closer()
+
+	cacher := db_mock.NewCacheKeeperIface(t)
+	repo := repository.NewPackageRepo(kit.DB, cacher)
+
+	testCases := []struct {
+		name                 string
+		wantErr              bool
+		expectedErr          error
+		expectedFunctionCall func()
+	}{
+		{
+			name:    "failed to get or lock",
+			wantErr: true,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(ctx, mock.Anything).Return("", nil, assert.AnError).Once()
+			},
+		},
+		{
+			name:        "cache was set to nil",
+			wantErr:     true,
+			expectedErr: repository.ErrNotFound,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(ctx, mock.Anything).Return("", nil, db.ErrCacheNil).Once()
+			},
+		},
+		{
+			name:        "too long waiting for cache",
+			wantErr:     true,
+			expectedErr: repository.ErrTimeout,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(ctx, mock.Anything).Return("", nil, db.ErrLockWaitingTooLong).Once()
+			},
+		},
+		{
+			name:    "failed to unmarshall",
+			wantErr: true,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(ctx, mock.Anything).Return("!", nil, nil).Once()
+			},
+		},
+		{
+			name:    "ok from cache",
+			wantErr: false,
+			expectedFunctionCall: func() {
+				cacher.EXPECT().GetOrLock(ctx, mock.Anything).Return(`[]`, nil, nil).Once()
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.expectedFunctionCall != nil {
+				tc.expectedFunctionCall()
+			}
+
+			res, err := repo.FindAllActivePackages(ctx)
 
 			if tc.wantErr {
 				require.Error(t, err)
--- internal/repository/usecase_adapter_test.go
+++ internal/repository/usecase_adapter_test.go
@@ -4,9 +4,15 @@ import (
 	"context"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
+	"github.com/luckyAkbar/atec/internal/model"
 	"github.com/luckyAkbar/atec/internal/repository"
 	"github.com/luckyAkbar/atec/internal/usecase"
+	db_mock "github.com/luckyAkbar/atec/mocks/internal_/db"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 )
 
 func TestUsecaeAdapter_UsecaseErrorUCAdapter(t *testing.T) {
@@ -40,19 +46,333 @@ func TestUsecaeAdapter_UsecaseErrorUCAdapter(t *testing.T) {
 	}
 }
 
-func TestUserRepositoryUCAdapter_Create(t *testing.T) {
+func TestChildRepositoryUCAdapter(t *testing.T) {
 	ctx := context.Background()
 	kit, closer := InitializeRepoTestKit(t)
 
 	defer closer()
 
-	// dbMock := kit.DBmock
+	dbMock := kit.DBmock
+	repo := repository.NewChildRepository(kit.DB)
+
+	adapter := repository.NewChildRepositoryUCAdapter(repo)
+
+	t.Run("Create", func(t *testing.T) {
+		dbMock.ExpectBegin()
+
+		dbMock.ExpectQuery("^INSERT INTO \"children\"").
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+
+		dbMock.ExpectCommit()
+
+		_, err := adapter.Create(ctx, usecase.RepoCreateChildInput{})
+		assert.NoError(t, err)
+	})
+
+	t.Run("FindByID", func(t *testing.T) {
+		id := uuid.New()
+
+		dbMock.ExpectQuery(`^SELECT .+ FROM "children"`).
+			WithArgs(id, 1).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(id))
+
+		_, err := adapter.FindByID(ctx, id)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Search", func(t *testing.T) {
+		offset := 11
+		limit := 1
+
+		dbMock.ExpectQuery(`^SELECT .+ FROM "children"`).
+			WithArgs(limit, offset).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()).AddRow(uuid.New()))
+
+		_, err := adapter.Search(ctx, usecase.RepoSearchChildInput{
+			Limit:  limit,
+			Offset: offset,
+		})
+		assert.NoError(t, err)
+	})
+
+	t.Run("Update", func(t *testing.T) {
+		childID := uuid.New()
+
+		dbMock.ExpectBegin()
+
+		dbMock.ExpectQuery("^UPDATE \"children\" SET").
+			WithArgs(sqlmock.AnyArg(), childID).
+			WillReturnRows(sqlmock.NewRows([]string{"id", "parent_user_id"}).AddRow(childID, uuid.New()))
+
+		dbMock.ExpectCommit()
+
+		_, err := adapter.Update(ctx, childID, usecase.RepoUpdateChildInput{})
+		assert.NoError(t, err)
+	})
+}
+
+func TestPackageRepositoryUCAdapter(t *testing.T) {
+	ctx := context.Background()
+	kit, closer := InitializeRepoTestKit(t)
+
+	defer closer()
+
+	dbMock := kit.DBmock
+	cacher := db_mock.NewCacheKeeperIface(t)
+	repo := repository.NewPackageRepo(kit.DB, cacher)
+
+	adapter := repository.NewPackageRepositoryUCAdapter(repo)
+
+	t.Run("Create - no controller", func(t *testing.T) {
+		dbMock.ExpectBegin()
+		dbMock.ExpectQuery(`^INSERT INTO "packages"`).
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+			WillReturnError(assert.AnError)
+		dbMock.ExpectRollback()
+
+		_, err := adapter.Create(ctx, usecase.RepoCreatePackageInput{})
+		require.Error(t, err)
+	})
+	t.Run("Create - with controller", func(t *testing.T) {
+		dbMock.ExpectBegin()
+		dbMock.ExpectQuery(`^INSERT INTO "packages"`).
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+			WillReturnError(assert.AnError)
+		dbMock.ExpectRollback()
+
+		_, err := adapter.Create(ctx, usecase.RepoCreatePackageInput{}, kit.DB)
+		require.Error(t, err)
+	})
+	t.Run("Create - invalid controller", func(t *testing.T) {
+		_, err := adapter.Create(ctx, usecase.RepoCreatePackageInput{}, 1)
+		require.Error(t, err)
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		id := uuid.New()
+
+		cacher.EXPECT().AcquireLock(mock.Anything).Return(nil, assert.AnError).Once()
+
+		err := adapter.Delete(ctx, id)
+		require.Error(t, err)
+	})
+
+	t.Run("FindAllActivePackages", func(t *testing.T) {
+		cacher.EXPECT().GetOrLock(ctx, string(repository.AllActivePackageCacheKey)).
+			Return("", nil, assert.AnError).Once()
+
+		_, err := adapter.FindAllActivePackages(ctx)
+		require.Error(t, err)
+	})
+
+	t.Run("FindByID", func(t *testing.T) {
+		id := uuid.New()
+
+		cacher.EXPECT().GetOrLock(ctx, mock.Anything).
+			Return("", nil, assert.AnError).Once()
+
+		_, err := adapter.FindByID(ctx, id)
+		require.Error(t, err)
+	})
+
+	t.Run("FindOldestActiveAndLockedPackage", func(t *testing.T) {
+		dbMock.ExpectQuery(`^SELECT .+ FROM "packages"`).
+			WithArgs(true, true, 1).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+
+		_, err := adapter.FindOldestActiveAndLockedPackage(ctx)
+		require.NoError(t, err)
+	})
+
+	t.Run("Search", func(t *testing.T) {
+		dbMock.ExpectQuery(`^SELECT .+ FROM "packages"`).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+
+		_, err := adapter.Search(ctx, usecase.RepoSearchPackageInput{})
+		require.NoError(t, err)
+	})
+
+	t.Run("Update - no controller", func(t *testing.T) {
+		id := uuid.New()
+
+		cacher.EXPECT().AcquireLock(mock.Anything).Return(nil, assert.AnError).Once()
+
+		_, err := adapter.Update(ctx, id, usecase.RepoUpdatePackageInput{})
+		require.Error(t, err)
+	})
+
+	t.Run("Update - with controller", func(t *testing.T) {
+		id := uuid.New()
+
+		cacher.EXPECT().AcquireLock(mock.Anything).Return(nil, assert.AnError).Once()
+
+		_, err := adapter.Update(ctx, id, usecase.RepoUpdatePackageInput{}, kit.DB)
+		require.Error(t, err)
+	})
+
+	t.Run("Update - err", func(t *testing.T) {
+		id := uuid.New()
+
+		_, err := adapter.Update(ctx, id, usecase.RepoUpdatePackageInput{}, 1)
+		require.Error(t, err)
+	})
+}
+
+func TestResultRepositoryUCAdapter(t *testing.T) {
+	ctx := context.Background()
+	kit, closer := InitializeRepoTestKit(t)
+
+	defer closer()
+
+	dbMock := kit.DBmock
+	repo := repository.NewResultRepository(kit.DB)
+
+	adapter := repository.NewResultRepositoryUCAdapter(repo)
+
+	t.Run("Create", func(t *testing.T) {
+		dbMock.ExpectBegin()
+
+		dbMock.ExpectQuery("^INSERT INTO \"results\"").
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+
+		dbMock.ExpectCommit()
+
+		_, err := adapter.Create(ctx, usecase.RepoCreateResultInput{})
+		require.NoError(t, err)
+	})
+
+	t.Run("FindAllUserHistory", func(t *testing.T) {
+		dbMock.ExpectQuery(`^SELECT .+ FROM "results"`).
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()).AddRow(uuid.New()))
+
+		_, err := adapter.FindAllUserHistory(ctx, usecase.RepoFindAllUserHistoryInput{})
+		require.NoError(t, err)
+	})
+
+	t.Run("FindByID", func(t *testing.T) {
+		dbMock.ExpectQuery(`^SELECT .+ FROM "results"`).
+			WithArgs(sqlmock.AnyArg(), 1).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+
+		_, err := adapter.FindByID(ctx, uuid.New())
+		require.NoError(t, err)
+	})
+
+	t.Run("Search", func(t *testing.T) {
+		dbMock.ExpectQuery(`^SELECT .+ FROM "results"`).
+			WillReturnError(assert.AnError)
+
+		_, err := adapter.Search(ctx, usecase.RepoSearchResultInput{})
+		require.Error(t, err)
+	})
+}
+
+func TestUserRepositoryUCAdapter(t *testing.T) {
+	ctx := context.Background()
+	kit, closer := InitializeRepoTestKit(t)
+
+	defer closer()
+
+	dbMock := kit.DBmock
 	repo := repository.NewUserRepository(kit.DB)
 
 	adapter := repository.NewUserRepositoryUCAdapter(repo)
 
-	t.Run("the tx controller is not gorm", func(t *testing.T) {
-		_, err := adapter.Create(ctx, usecase.RepoCreateUserInput{}, 1)
+	t.Run("Create - the tx controller is not gorm", func(t *testing.T) {
+		_, err := adapter.Create(ctx, usecase.RepoCreateUserInput{}, &model.User{})
 		assert.Error(t, err)
+	})
+
+	t.Run("Create - the tx controller is not gorm", func(t *testing.T) {
+		_, err := adapter.Create(ctx, usecase.RepoCreateUserInput{}, kit.DB)
+		assert.Error(t, err)
+	})
+
+	t.Run("Create - tx controller wasn't supplied", func(t *testing.T) {
+		dbMock.ExpectBegin()
+
+		dbMock.ExpectQuery("^INSERT INTO \"users\"").
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg()).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+
+		dbMock.ExpectCommit()
+
+		_, err := adapter.Create(ctx, usecase.RepoCreateUserInput{})
+		assert.NoError(t, err)
+	})
+
+	t.Run("FindByEmail - ok", func(t *testing.T) {
+		email := "email"
+
+		dbMock.ExpectQuery(`^SELECT .+ FROM "users"`).
+			WithArgs(email, 1).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+
+		_, err := adapter.FindByEmail(ctx, email)
+		assert.NoError(t, err)
+	})
+
+	t.Run("FindByID - ok", func(t *testing.T) {
+		id := uuid.New()
+
+		dbMock.ExpectQuery(`^SELECT .+ FROM "users"`).
+			WithArgs(id, 1).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(id))
+
+		_, err := adapter.FindByID(ctx, id)
+		assert.NoError(t, err)
+	})
+
+	t.Run("IsAdminAccountExists - ok", func(t *testing.T) {
+		dbMock.ExpectQuery(`^SELECT .+ FROM "users"`).
+			WithArgs(model.RolesAdministrator, 1).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()))
+
+		_, err := adapter.IsAdminAccountExists(ctx)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Search - ok", func(t *testing.T) {
+		offset := 11
+		limit := 1
+		role := model.RolesAdministrator
+
+		dbMock.ExpectQuery(`^SELECT .+ FROM "users"`).
+			WithArgs(role, limit, offset).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.New()).AddRow(uuid.New()))
+
+		_, err := adapter.Search(ctx, usecase.RepoSearchUserInput{
+			Role:   role,
+			Limit:  limit,
+			Offset: offset,
+		})
+		assert.NoError(t, err)
+	})
+
+	t.Run("Update - ok", func(t *testing.T) {
+		userID := uuid.New()
+		email := "test@email.com"
+		password := "password1"
+		username := "username"
+		isActive := true
+
+		dbMock.ExpectBegin()
+
+		dbMock.ExpectQuery("^UPDATE \"users\" SET").
+			WithArgs(email, isActive, password, sqlmock.AnyArg(), userID).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(userID))
+
+		dbMock.ExpectCommit()
+
+		_, err := adapter.Update(ctx, userID, usecase.RepoUpdateUserInput{
+			Email:    email,
+			Password: password,
+			Username: username,
+			IsActive: &isActive,
+		})
+		assert.NoError(t, err)
 	})
 }
